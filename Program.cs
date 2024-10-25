efee78a16bf27f4f857a345838ef5746c0b9b1a8
Andrey <poetflash@users.noreply.github.com>
2024-10-25 15:46:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+using ConsoleApp1;
+
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		//BenchmarkRunner.Run<MyBenchmark>();
+		ExecutionTime.Run();
+	}
+}
